d2938d496e9a567bf966d2e253d2a21aafa7e81f
yellow <yellow@yellows-iMac-Pro.local>
2025-02-04 17:26:58 +0800

    add another bookmarks
    Signed-off-by: yellow <yellow@yellows-iMac-Pro.local>

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -35,3 +35,19 @@ require("yamb"):setup {
   path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
         (os.getenv("HOME") .. "/.config/yazi/bookmark"),
 }
+
+require("bookmarks"):setup({
+	last_directory = { enable = false, persist = false },
+	persist = "all",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
