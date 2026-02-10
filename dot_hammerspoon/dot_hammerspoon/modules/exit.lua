aecbc6dcecd4b808c66ae30b493f80fb82b79692
Sammy Huang <sammy.huang@zilliz.com>
2026-02-10 16:29:55 +0800

    Add .hammerspoon/Spoons/.DS_Store Remove .hammerspoon/Spoons/ReloadConfiguration.spoon Add .hammerspoon/.DS_Store Remove .hammerspoon/.hammerspoon/init.lua Remove .hammerspoon/.hammerspoon/modules/applications.lua Remove .hammerspoon/.hammerspoon/modules/exit.lua Remove .hammerspoon/.hammerspoon/modules/helloworld.lua Remove .hammerspoon/.hammerspoon/modules/inputmethods.lua Remove .hammerspoon/.hammerspoon/modules/reload.lua Remove .hammerspoon/.hammerspoon/modules/shortcut.lua Remove .hammerspoon/.hammerspoon/modules/window.lua Change attributes of .hammerspoon/Spoons/ReloadConfiguration.spoon/docs.json Change attributes of .hammerspoon/Spoons/ReloadConfiguration.spoon/init.lua Change attributes of .hammerspoon/readme.txt

--- dot_hammerspoon/dot_hammerspoon/modules/exit.lua
+++
@@ -1,6 +0,0 @@
-
-hs.hotkey.bind({"alt"}, "Q", function()
-  hs.console.clearConsole()
-  hs.alert.show("Hammerspoon Exiting")
-  hs.timer.doAfter(0.5, function() hs.application.applicationForPID(hs.processInfo["processID"]):kill() end)
-end)
--- dot_hammerspoon/dot_hammerspoon/modules/reload.lua
+++
@@ -1,4 +0,0 @@
-hs.loadSpoon("ReloadConfiguration")
-spoon.ReloadConfiguration:start()
-hs.alert.show("Config loaded")
-
