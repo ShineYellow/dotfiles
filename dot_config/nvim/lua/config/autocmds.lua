f74309ed183eca3c9d8a4aa0f9c7e3ad1f1d4f13
Sammy Huang <sammy.huang@zilliz.com>
2026-02-11 22:45:11 +0800

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -5,5 +5,4 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
---
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")j--
