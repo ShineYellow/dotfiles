cf434ac2df52789bd1d80452c43dd40b1f8f145f
Sammy Huang <sammy.huang@zilliz.com>
2026-02-15 21:03:09 +0800

    Update .config/nvim/lua/plugins/bufferline.lua Add .config/nvim/lua/plugins/harpoon2.lua

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,4 +1,14 @@
 return {
-  -- disable bufferline
-  { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "akinsho/bufferline.nvim",
+    enabled = true,
+    opts = {
+      options = {
+        custom_filter = function(buf, buf_nums)
+          -- Only show the current buffer
+          return buf == vim.api.nvim_get_current_buf()
+        end,
+      },
+    },
+  },
 }
